8518088aef9c69d3a0fd6101f7a8cd0af9b300ba
huyuyuan <yuan0141@gmail.com>
2017-03-22 15:40:41 +0800

    新增切换城市和手动更新天气的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Huyuyuan on 2017/3/0020.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
